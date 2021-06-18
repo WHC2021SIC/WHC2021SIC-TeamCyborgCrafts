ad3e0909f17fd28faa21a6ab5828505ade6b8014
unknown <mwest.lazerade@gmail.com>
2021-06-17 20:19:33 -0600

    tested training modules

--- src/letters.py
+++ src/letters.py
@@ -1,16 +1,51 @@
-# Player enters placement (row, column, orientation) of ships on their grid
-# Player enters attack
+
 from time import sleep
 from syntacts import *
-from ship import Minesweeper, Battleship, Destroyer, Submarine
-from grid import Grid, OccupantType
-from weapon import Cannon, Laser
-from enum import Enum
+
 
 
 GAIN = 1
-SAMPLE_PERIOD = .25
+SAMPLE_PERIOD = 1
 FREQUENCY = 220
+
+char_mapping_dict = {'A': [1, 1, 1, 0, 1, 1, 1],
+                     'B': [1, 1, 1, 1, 1, 1, 1],
+                     'C': [1, 0, 0, 1, 1, 1, 0],
+                     'D': [0, 1, 1, 1, 1, 0, 1],
+                     'E': [1, 0, 0, 1, 1, 1, 1],
+                     'F': [1, 0, 0, 0, 1, 1, 1],
+                     'G': [1, 0, 1, 1, 1, 1, 1],
+                     'H': [0, 0, 1, 0, 1, 1, 1],
+                     'I': [0, 1, 1, 0, 0, 0, 0],
+                     'J': [0, 1, 1, 1, 1, 0, 0],
+                     'K': [0, 1, 1, 0, 1, 1, 0],
+                     'L': [0, 0, 0, 1, 1, 1, 0],
+                     'M': [1, 0, 0, 1, 0, 0, 0],
+                     'N': [0, 0, 1, 0, 1, 0, 1],
+                     'O': [1, 1, 1, 1, 1, 1, 0],
+                     'P': [1, 1, 0, 0, 1, 1, 1],
+                     'Q': [1, 1, 1, 0, 0, 1, 1],
+                     'R': [0, 0, 0, 0, 1, 0, 1],
+                     'S': [1, 0, 1, 1, 0, 1, 1],
+                     'T': [0, 0, 0, 1, 1, 1, 1],
+                     'U': [0, 1, 1, 1, 1, 1, 0],
+                     'V': [0, 1, 0, 1, 0, 1, 0],
+                     'W': [1, 0, 0, 1, 0, 0, 1],
+                     'X': [0, 1, 1, 0, 1, 1, 0],
+                     'Y': [0, 1, 1, 1, 0, 1, 1],
+                     'Z': [1, 1, 0, 1, 1, 0, 1]}
+
+
+def print_mapping(value):
+    if value.isalnum() and value in char_mapping_dict:
+        array = char_mapping_dict[value]
+        print(" " + str(array[0]) + " ")
+        print(str(array[5]) + " " + str(array[1]))
+        print(" " + str(array[6]) + " ")
+        print(str(array[4]) + " " + str(array[2]))
+        print(" " + str(array[3]) + " ")
+    else:
+        print("Couldn't print: " + str(value))
 
 
 if __name__ == "__main__":
@@ -23,69 +58,26 @@ if __name__ == "__main__":
     s.open()
     # Create a base sine wave for motors
     base_wave = Sine(FREQUENCY)
+    sequence = base_wave * Envelope(SAMPLE_PERIOD, 1)
 
     print("Welcome to the Cyborg Crafts' training module - recognizing letters with your tongue!")
     while 1:
         try:
-            usrinput = int(input("Type a letter between A-Z you want to read with your tongue: "))
+            usrinput = str(input("Type a letter between A-Z you want to read with your tongue: "))
+            usrinput = usrinput.upper()
             print(usrinput)
+            if len(usrinput) == 1 and usrinput.isalnum() and usrinput in char_mapping_dict:
+                print_mapping(usrinput)
+                playable_channels = char_mapping_dict[usrinput]
+                for channel in range(len(playable_channels)):
+                    if playable_channels[channel]:
+                        print("Would play sequence for channel: " + str(channel))
+                        s.play(channel, sequence)
+                # Wait while sequence plays for SAMPLE_PERIOD seconds
+                sleep(sequence.length * .9)
+            else:
+                print("Couldn't process input: " + str(usrinput))
+
+
         except ValueError:
           print("invalid input")
-        continue
-        if usrinput == A:
-            s.play[0]
-        if usrinput == B:
-            s.play[1]
-        if usrinput == C:
-            s.play[2]
-        if usrinput == D:
-            s.play[3]
-        if usrinput == E:
-            s.play[4]
-        if usrinput == F:
-            s.play[5]
-        if usrinput == G:
-            s.play[6]
-        if usrinput == H:
-            s.play[7]
-        if usrinput == I:
-            s.play[8]
-        if usrinput == J:
-            s.play[9]
-        if usrinput == K:
-            s.play[10]
-        if usrinput == L:
-            s.play[11]
-        if usrinput == M:
-            s.play[12]
-        if usrinput == N:
-            s.play[13]
-        if usrinput == O:
-            s.play[14]
-        if usrinput == P:
-            s.play[15]
-        if usrinput == Q:
-            s.play[16]
-        if usrinput == R:
-            s.play[17]
-        if usrinput == S:
-            s.play[18]
-        if usrinput == T:
-            s.play[19]
-        if usrinput == U:
-            s.play[20]
-        if usrinput == V:
-            s.play[21]
-        if usrinput == W:
-            s.play[22]
-        if usrinput == X:
-            s.play[23]
-        if usrinput == Y:
-            s.play[24]
-        if usrinput == Z:
-            s.play[25]
-        else:
-            print("Invalid input")
-
-
-
--- src/numbers.py
+++ src/numbers.py
@@ -1,5 +1,4 @@
-# Player enters placement (row, column, orientation) of ships on their grid
-# Player enters attack
+
 from time import sleep
 from syntacts import *
 from ship import Minesweeper, Battleship, Destroyer, Submarine
@@ -9,8 +8,32 @@ from enum import Enum
 
 
 GAIN = 1
-SAMPLE_PERIOD = .25
+SAMPLE_PERIOD = 1
 FREQUENCY = 220
+
+
+char_mapping_dict = {'1': [0, 1, 1, 0, 0, 0, 0],
+                     '2': [1, 1, 0, 1, 1, 0, 1],
+                     '3': [1, 1, 1, 1, 0, 0, 1],
+                     '4': [0, 1, 1, 0, 0, 1, 1],
+                     '5': [1, 0, 1, 1, 0, 1, 1],
+                     '6': [1, 0, 1, 1, 1, 1, 1],
+                     '7': [1, 1, 1, 0, 0, 0, 0],
+                     '8': [1, 1, 1, 1, 1, 1, 1],
+                     '9': [1, 1, 1, 0, 0, 1, 1],
+                     '0': [1, 1, 1, 1, 1, 1, 0]}
+
+
+def print_mapping(value):
+    if value.isalnum() and value in char_mapping_dict:
+        array = char_mapping_dict[value]
+        print(" " + str(array[0]) + " ")
+        print(str(array[5]) + " " + str(array[1]))
+        print(" " + str(array[6]) + " ")
+        print(str(array[4]) + " " + str(array[2]))
+        print(" " + str(array[3]) + " ")
+    else:
+        print("Couldn't print: " + str(value))
 
 
 if __name__ == "__main__":
@@ -23,37 +46,26 @@ if __name__ == "__main__":
     s.open()
     # Create a base sine wave for motors
     base_wave = Sine(FREQUENCY)
+    sequence = base_wave * Envelope(SAMPLE_PERIOD, 1)
 
-    print("Welcome to the Cyborg Crafts' training module - recognizing number with your tongue!")
+    print("Welcome to the Cyborg Crafts' training module - recognizing numbers with your tongue!")
     while 1:
         try:
-            usrinput = int(input("Type a number between 0-9 you want to read with your tongue: "))
+            usrinput = str(input("Type a number between 0-9 you want to read with your tongue: "))
+            usrinput = usrinput.upper()
             print(usrinput)
+            if len(usrinput) == 1 and usrinput.isalnum() and usrinput in char_mapping_dict:
+                print_mapping(usrinput)
+                playable_channels = char_mapping_dict[usrinput]
+                for channel in range(len(playable_channels)):
+                    if playable_channels[channel]:
+                        print("Would play sequence for channel: " + str(channel))
+                        s.play(channel, sequence)
+                # Wait while sequence plays for SAMPLE_PERIOD seconds
+                sleep(sequence.length * .9)
+            else:
+                print("Couldn't process input: " + str(usrinput))
+
+
         except ValueError:
           print("invalid input")
-        continue
-        if usrinput == 0:
-            s.play[0]
-        if usrinput == 1:
-            s.play[1]
-        if usrinput == 2:
-            s.play[2]
-        if usrinput == 3:
-            s.play[3]
-        if usrinput == 4:
-            s.play[4]
-        if usrinput == 5:
-            s.play[5]
-        if usrinput == 6:
-            s.play[6]
-        if usrinput == 7:
-            s.play[7]
-        if usrinput == 8:
-            s.play[8]
-        if usrinput == 9:
-            s.play[9]
-        else:
-            print("Invalid input")
-
-
-
